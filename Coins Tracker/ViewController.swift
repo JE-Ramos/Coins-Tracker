672e38f9d31b4dae5e699d10eaaa389b55870e66
John Ernest Ramos <john.ramos@erni.ph>
2018-02-04 11:33:01 +0800

    Initial Commit

---
+++ Coins Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coins Tracker
+//
+//  Created by John Ernest Ramos on 2/4/18.
+//  Copyright © 2018 John Ernest Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
